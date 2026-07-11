91aa343ca7410fd2c23bb285525abbb2b0050391
Satyam Kumar Yadav <2k24.cs1n.2413913@gmail.com>
2026-07-11 21:51:35 +0530

    today notes

---
+++ Part3/condition.js
@@ -0,0 +1,9 @@
+// checking condition if a number is greater than another number
+let num1 = 5;
+let num2 = 8;
+let object;
+if (num1 < num2) {
+  console.log('num1 is greater than num2');
+} else {
+  console.log('num1 is not grater than num2');
+}
